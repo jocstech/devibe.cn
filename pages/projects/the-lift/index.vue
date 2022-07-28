b83bf942f53d77e441a1779ab9e19d54cff17a66
Joseph Fang <aionv@qq.com>
2022-07-29 02:26:12 +0800

    canvas animation

--- pages/projects/the-lift/index.vue
+++ pages/projects/the-lift/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col place-items-center p-6">
     <h1 class="text-3xl">Project of 'The Lift'</h1>
-    <div class="view shadow">
+    <div class="view border">
       <canvas ref="canvasRef"></canvas>
     </div>
   </div>
@@ -13,6 +13,11 @@ import { ref, onMounted, onUnmounted } from 'vue';
 const config = {
   width: 600,
   height: 600,
+};
+
+const realworld = {
+  width: 0,
+  height: 0,
 };
 
 const canvasRef = ref(null);
@@ -46,24 +51,53 @@ const object2 = {
   },
 };
 
+const object3 = {
+  x: -100,
+  y: -100,
+  r: 18,
+  color: {
+    r: 100,
+    g: 100,
+    b: 100,
+    a: 0.9,
+  },
+};
+
 const setup = () => {
+  realworld.width =
+    document.body.clientWidth || window.innerWidth || config.width;
+  realworld.height = config.height;
+
   canvas = canvasRef.value;
   ctx = canvas.getContext('2d');
   const ratio = window.devicePixelRatio || 1;
-  const width = document.body.clientWidth || window.innerWidth || 600;
 
   // setups
-  canvas.width = width * ratio;
+  canvas.width = realworld.width * ratio;
   canvas.height = config.height * ratio;
-  canvas.style.width = `${width}px`;
+  canvas.style.width = `${realworld.width}px`;
   canvas.style.height = `${config.height}px`;
   ctx.scale(ratio, ratio);
+
+  // listeners
+  window.addEventListener('mousemove', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    object3.x = event.clientX - rect.left;
+    object3.y = event.clientY - rect.top;
+  });
+
+  window.addEventListener('mousedown', (event) => {
+    object3.r = 60;
+  });
+
+  window.addEventListener('mouseup', (event) => {
+    object3.r = 18;
+  });
 };
 
 const draw = () => {
   // clear canvas
-  ctx.fillStyle = '#ffffff';
-  ctx.fillRect(0, 0, config.width, config.height);
+  ctx.clearRect(0, 0, realworld.width, realworld.height);
 
   // draw object 1
   ctx.fillStyle = `rgba(${object1.color.r}, ${object1.color.g}, ${object1.color.b}, ${object1.color.a})`;
@@ -72,6 +106,15 @@ const draw = () => {
   // draw object 2
   ctx.fillStyle = `rgba(${object2.color.r}, ${object2.color.g}, ${object2.color.b}, ${object2.color.a})`;
   ctx.fillRect(object2.x, object2.y, object2.w, object2.h);
+
+  // draw object 3
+  ctx.beginPath();
+  ctx.arc(object3.x, object3.y, object3.r, 0, Math.PI * 2, true);
+  ctx.fillStyle = `rgba(${object3.color.r}, ${object3.color.g}, ${object3.color.b}, ${object3.color.a})`;
+  ctx.strokeStyle = '#000';
+  ctx.fill();
+  ctx.stroke();
+  ctx.closePath(); //关闭路径
 };
 
 const action = () => {
@@ -87,7 +130,7 @@ onMounted(() => {
   animation = setInterval(() => {
     action();
     draw();
-  }, 10);
+  });
 });
 
 onUnmounted(() => clearInterval(animation));
