e94bdc2114a056f6802ab8424c009e35e9d1c257
Joseph Fang <aionv@qq.com>
2022-07-29 00:22:55 +0800

    new header

---
+++ components/TheHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="bg-indigo-700 text-white">
+    <nav class="px-2 py-3">
+      <ul class="flex justify-center gap-3 capitalize">
+        <li
+          class="hover:text-blue-200 cursor-pointer"
+          v-for="nav of navs"
+          :key="nav.text"
+        >
+          <nuxt-link :to="nav.to">{{ nav.text }}</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+const navs = ref([
+  {
+    to: '/',
+    text: 'home',
+  },
+  {
+    to: '/projects',
+    text: 'projects',
+  },
+  {
+    to: '/projects/the-lift',
+    text: 'the lift',
+  },
+  {
+    to: '/projects/the-lift',
+    text: 'project 2',
+  },
+  {
+    to: '/projects/the-lift',
+    text: 'project 3',
+  },
+  {
+    to: '/projects/the-lift',
+    text: 'project 4',
+  },
+  {
+    to: '/projects/the-lift',
+    text: 'project 5',
+  },
+]);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const count = ref(0);
 const increment = () => count.value++;
 const reduction = () => count.value--;
