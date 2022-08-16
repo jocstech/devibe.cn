4df2262bc4615fae02e90f5298ebe52d59a37dfb
Joseph Fang <aionv@qq.com>
2022-08-16 22:03:38 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,15 +10,9 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'The Best Developer Online Community in China',
-      link: [
-        {
-          rel: 'stylesheet',
-          href: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.css',
-        },
-      ],
       charset: 'utf-8',
       viewport:
-        'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"',
+        'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
       noscript: [{ children: 'Javascript is required!' }],
     },
   },
